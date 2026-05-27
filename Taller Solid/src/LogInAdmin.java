988a71b2add4ef61676757c60ca7c256fe477401
MariaPacha24 <mjpacha@espol.edu.ec>
2026-05-27 14:56:50 -0500

    Sección B
    Se encontro las violaciones de los principios LSP y SRP

--- Taller Solid/src/LogInAdmin.java
+++ Taller Solid/src/LogInAdmin.java
@@ -1,5 +1,7 @@
 public class LogInAdmin extends LogIn {
     private boolean userIsAdmin;
+    //Violación del principio LSP, en este caso userIsAdmin extiende de LogIn pero si en algun momento logInAdmin
+    //pasa a sustituir a su clase padre creara errores por la logica que no tiene que ver una con la otra
     @Override
     public void log (User user) {
         this.userIsAdmin = verifyIfTheUserIsAdmin(user);
---
+++ Taller Solid/src/UsuarioDataBase.java
@@ -0,0 +1,5 @@
+
+public class UsuarioDataBase {
+    public void insertUser(User user){
+    }
+}
